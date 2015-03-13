d9a120dedaf1fa1e196cb06a5cbe3f4290858983
Ayan Saha <ayan.saha91@gmail.com>
2015-03-13 16:24:22 +0530

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,25 @@ def show
  end
   def create
      @article = Article.new(article_params)
- 
-  @article.save
-  redirect_to @article
-   end
+     @article.save
+      redirect_to @article
+  end
   private
   def article_params
     params.require(:article).permit(:title, :text)
   end
+def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+def destroy
+ @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
 end
